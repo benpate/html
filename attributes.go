e7e1d143334e3838e0f9c627d5c70b5b5651fe7a
Ben Pate <ben@pate.org>
2021-04-06 09:09:26 -0600

    Adding "type" attribute

--- attributes.go
+++ attributes.go
@@ -15,6 +15,11 @@ func (element *Element) ID(value string) *Element {
 	return element.Attr("id", value)
 }
 
+// Type adds a "type" attribute to the Element
+func (element *Element) Type(value string) *Element {
+	return element.Attr("type", value)
+}
+
 // Name adds a "name" attribute to the Element
 func (element *Element) Name(value string) *Element {
 	return element.Attr("name", value)
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/benpate/html
 go 1.15
 
 require (
-	github.com/benpate/convert v0.8.1
+	github.com/benpate/convert v0.9.0
 	github.com/stretchr/testify v1.7.0
 )
